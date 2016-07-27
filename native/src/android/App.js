b1ca9683a3d148f99fdf21c1bc44359653e8d448
Shea Hawkins <Shea Hawkins>
2016-07-27 11:18:11 -0700

    Refactor Tasklist into a separate component

--- native/src/android/App.js
+++ native/src/android/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  View,
-  Text,
+  Navigator,
   DrawerLayoutAndroid
 } from 'react-native';
 import MenuBar from './resources/stateless/MenuBar';
+import TaskList from './resources/TaskList/TaskListView';
 
 class App extends Component {
   render() {
@@ -13,12 +12,8 @@ class App extends Component {
     <DrawerLayoutAndroid
       drawerWidth={300}
       drawerPosition={DrawerLayoutAndroid.positions.Right}
-      renderNavigationView={MenuBar}>
-      {/* Temporary 'Main' component */}
-      <View style={{flex: 1, alignItems: 'center'}}>
-        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Hello</Text>
-        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
-      </View>
+      renderNavigationView={() => <MenuBar />}>
+      <TaskList />
     </DrawerLayoutAndroid>
     );
   }
---
+++ native/src/android/resources/TaskList/TaskListView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default class TaskList extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>Task</Text>
+        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
+      </View>
+    );
+  }
+}
