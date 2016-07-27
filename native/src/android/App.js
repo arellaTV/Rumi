b44b8a5688ff08fc4ed7975f92d175cb26a3e110
Shea Hawkins <Shea Hawkins>
2016-07-27 12:21:12 -0700

    Initialize Navigation State and reducer

--- native/src/android/App.js
+++ native/src/android/App.js
@@ -1,20 +1,68 @@
 import React, { Component } from 'react';
 import {
-  Navigator,
-  DrawerLayoutAndroid
+  DrawerLayoutAndroid,
+  NavigationExperimental as Navigator,
+  TouchableHighlight,
+  Text
 } from 'react-native';
+
 import MenuBar from './resources/stateless/MenuBar';
 import TaskList from './resources/TaskList/TaskListView';
 
-class App extends Component {
+class TappableRow extends Component {
   render() {
     return (
-    <DrawerLayoutAndroid
-      drawerWidth={300}
-      drawerPosition={DrawerLayoutAndroid.positions.Right}
-      renderNavigationView={() => <MenuBar />}>
-      <TaskList />
-    </DrawerLayoutAndroid>
+      <TouchableHighlight
+        underlayColor="#D0D0D0"
+        onPress={this.props.onPress}>
+        <Text>
+          {this.props.text}
+        </Text>
+      </TouchableHighlight>
+    )
+  }
+}
+
+class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      navigationState: {
+        index: 0,
+        routes: [{key: 'Task List'}]
+      }
+    };
+  }
+
+  _onNavigationChange(type) {
+    let {navigationState} = this.state;
+    switch(type) {
+      case 'push':
+        const route = {key: 'Route-' + Date.now()};
+        navigationState = NavigationStateUtils.push(navigationState, route);
+        break;
+      case 'pop':
+        navigationState = NavigationStateUtils.pop(navigationState);
+        break;
+    }
+    if (this.state.navigationState !== navigationState) {
+      this.setState(navigationState);
+    }
+  }
+
+  render() {
+    return (
+      <DrawerLayoutAndroid
+        drawerWidth={300}
+        drawerPosition={DrawerLayoutAndroid.positions.Right}
+        renderNavigationView={() => <MenuBar />}>
+        <Text>
+          Route: {this.state.navigationState.routes[this.state.navigationState.index].key}
+        </Text>
+        <TappableRow text="Tap me to go to next screen" onPress={() => console.log('pressme')}/>
+        <TappableRow text="Tap me to go back" onPress={() => console.log('pressme')}/>
+        <TaskList />
+      </DrawerLayoutAndroid>
     );
   }
 }
