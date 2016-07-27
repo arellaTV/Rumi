d83284263eeafda10303f6a71dd5ef03f40048bf
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-27 15:31:20 -0700

    Add basic styling for task list

--- native/src/android/resources/taskListView/taskListView.js
+++ native/src/android/resources/taskListView/taskListView.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
 import {
   ListView,
+  StyleSheet,
   Text,
+  TouchableNativeFeedback,
   View
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  taskList: {
+    backgroundColor: '#EAEAEA',
+    flex: 2,
+    alignItems: 'stretch',
+  },
+  taskCard: {
+    backgroundColor: '#fff',
+    flex: 2,
+    justifyContent: 'space-around',
+  },
+  titleText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  baseText: {
+    fontSize: 20,
+  },
+});
 
 
 class TaskListView extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
       dataSource: ds.cloneWithRows([
         {"id": 1, "name": "Do Dishes", "due": "Tomorrow"},
@@ -17,17 +40,22 @@ class TaskListView extends Component {
       ])
     };
   }
+
   render() {
     return (
-      <View style={{paddingTop: 22}}>
+      <View style={styles.taskList}>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={(rowData) => 
-            <Text>
-              {rowData.id} 
-              {rowData.name} 
-              {rowData.due}
-            </Text>
+            <TouchableNativeFeedback
+              onPress={this._onPressButton}
+              background={TouchableNativeFeedback.SelectableBackground()}>
+              <View style={styles.taskCard}>
+                <Text style={styles.titleText}> {rowData.name} </Text>
+                <Text style={styles.baseText}> Due: {rowData.due} </Text>
+                <Text style={styles.baseText}> Last completed by NAME </Text>
+              </View>
+            </TouchableNativeFeedback>
           }
         />
       </View>
