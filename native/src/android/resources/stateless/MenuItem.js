5bcc65de3b4998505b43bb1d75993f6b924b6986
Jennifer Ong <jenn.r.ong@gmail.com>
2016-07-30 15:09:52 -0700

    Fix styling for active menu button

--- native/src/android/resources/stateless/MenuItem.js
+++ native/src/android/resources/stateless/MenuItem.js
@@ -10,13 +10,13 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 export default (props) => {
   console.log(props);
   return (
-    <View>
+    <View >
         <TouchableNativeFeedback
           onPress={props.onPushRoute.bind(null, {targetView: props.targetView})}
           background={TouchableNativeFeedback.SelectableBackground()}>
           <View style={props.targetView === props.activeView ? styles.indivMenuActive : styles.indivMenu}>
             <Icon name={props.icon} size={40} color={props.targetView === props.activeView ? '#EAEAEA' : "#7AD05D"} />
-            <Text style={styles.menuText}>{props.title}</Text>
+            <Text style={styles.menuText}>{props.title}   </Text>
           </View>
         </TouchableNativeFeedback>
     </View>
